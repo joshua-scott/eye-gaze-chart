b6c452c2a4bbc1f043f4c50b90cfb9c219bb446b
Joshua Scott <jscott313@gmail.com>
2019-11-24 20:41:39 +0200

    Add HeatmapGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import "./App.css";
 import getData from "./helpers/getData";
 import RawDataTable from "./components/RawDataTable";
-
-const tableCellStyles = {
-  textAlign: "left",
-  borderLeft: "2px solid #333",
-  borderRight: "2px solid #333"
-};
+import HeatmapGrid from "./components/HeatmapGrid";
 
 const App: React.FC = () => {
   const {
@@ -23,35 +18,15 @@ const App: React.FC = () => {
     everyString
   });
 
+  console.log(`lol`);
+
   return (
     <div className="App">
       <main>
         {/* Grid of all coordinates, where the transparency varies on frequency (each one's transparency is a function of the % of total grid views) */}
+        <HeatmapGrid data={coordinateData} />
 
         <RawDataTable lineData={lineData} />
-        {/* Table of data */}
-        {/* <table>
-          <thead>
-            <tr>
-              <th style={tableCellStyles}>lineNumber</th>
-              <th style={tableCellStyles}>lineString</th>
-              <th style={tableCellStyles}>coordinatesString</th>
-              <th style={tableCellStyles}>coordinatesArray</th>
-            </tr>
-          </thead>
-          <tbody>
-            {lineData.map(line => {
-              return (
-                <tr key={line.lineNumber}>
-                  <td style={tableCellStyles}>{line.lineNumber}</td>
-                  <td style={tableCellStyles}>{line.lineString}</td>
-                  <td style={tableCellStyles}>{line.coordinatesString}</td>
-                  <td style={tableCellStyles}>{line.coordinatesArray}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table> */}
       </main>
     </div>
   );
---
+++ src/components/HeatmapGrid.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeatmapGrid: React.FC = ({ data }) => {
+  return (
+    <div>
+      <p>HeatmapGrid</p>
+      <p>{data.toString()}</p>
+    </div>
+  );
+};
+
+export default HeatmapGrid;
